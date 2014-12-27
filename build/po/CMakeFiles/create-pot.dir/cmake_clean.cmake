76d3c811c167210411be89975201f5592220d01a
Joel Almeida <aullidolunar@gmail.com>
2014-12-27 13:23:15 -0800

    update po.es

---
+++ build/po/CMakeFiles/create-pot.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create-pot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/create-pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/po/CMakeFiles/locales.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/locales"
+  "../locale/es/LC_MESSAGES"
+  "../locale/es/LC_MESSAGES/gtk2-youtubedl.mo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/locales.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
