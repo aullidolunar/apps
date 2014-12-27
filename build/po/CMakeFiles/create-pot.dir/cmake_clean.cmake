278d6f81b2910e2f3aa547032b46b227f5a62f8d
Joel Almeida <aullidolunar@gmail.com>
2014-12-27 13:24:57 -0800

    update po.es

--- build/po/CMakeFiles/create-pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/create-pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/create-pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
